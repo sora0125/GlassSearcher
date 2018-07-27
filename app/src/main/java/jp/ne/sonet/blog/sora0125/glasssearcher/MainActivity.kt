d5c8597a31d345a8b63d9544ba3df8671cba334d
s-nagano <g.sora0125@gmail.com>
2018-07-28 00:13:59 +0900

    新規作成

---
+++ app/src/main/java/jp/ne/sonet/blog/sora0125/glasssearcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.ne.sonet.blog.sora0125.glasssearcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
